1c1ff2ae249a331b4738190ce6ec076eff32db72
Julia Bezrukova <julika7391@gmail.com>
2021-03-02 17:14:28 +0500

    added db init

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/sirupsen/logrus"
+	"messenger/db"
 	"messenger/helpers"
 	"messenger/server"
 )
@@ -9,7 +10,12 @@ import (
 func main() {
 	helpers.ConfigureLogrus()
 
-	s := server.Init()
+	dbClient, err := db.ConnectToPostgres()
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	s := server.Init(dbClient)
 
 	logrus.Info("Server is running on ", s.Addr)
 	logrus.Panic(s.ListenAndServe())
